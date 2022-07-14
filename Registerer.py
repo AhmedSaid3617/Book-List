fcf28f49e398b449248d57869bbb1051b90a445e
AhmedSaid3617 <said.ahmed3617@gmail.com>
2022-07-14 20:10:14 +0200

    Simplest version, works with excel.

---
+++ Registerer.py
@@ -0,0 +1,25 @@
+from Books import Book
+import openpyxl
+
+filename = "Records.xlsx"
+wb = openpyxl.load_workbook(filename)
+sheet = wb.active
+maxRow = sheet.max_row
+
+def Record(data):
+    sheet[f'A{data.id}'] = data.title
+    sheet[f'B{data.id}'] = data.author
+    sheet[f'C{data.id}'] = data.year
+
+another = 0
+while another != "no":
+    title = input("Enter book title:")
+    author = input("Enter book author:")
+    year = input("Enter book year:")
+    newBook = Book(maxRow+1, title, author, year)
+    Record(newBook)
+    another = input("Type \"no\" to stop.")
+
+
+wb.save(filename)
+print("done")
