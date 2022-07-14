44e03a7badef80cd0fd44dff592ad642b7b0090b
AhmedSaid3617 <said.ahmed3617@gmail.com>
2022-07-15 00:10:24 +0200

    This version has commands to create and delete book registries.

--- Registerer.py
+++ Registerer.py
@@ -4,22 +4,43 @@ import openpyxl
 filename = "Records.xlsx"
 wb = openpyxl.load_workbook(filename)
 sheet = wb.active
-maxRow = sheet.max_row
 
-def Record(data):
+def record(data):
     sheet[f'A{data.id}'] = data.title
     sheet[f'B{data.id}'] = data.author
     sheet[f'C{data.id}'] = data.year
 
-another = 0
-while another != "no":
+
+def create_book():
+    max_row = sheet.max_row
     title = input("Enter book title:")
     author = input("Enter book author:")
     year = input("Enter book year:")
-    newBook = Book(maxRow+1, title, author, year)
-    Record(newBook)
-    another = input("Type \"no\" to stop.")
+    new_book = Book(max_row+1, title, author, year)
+    record(new_book)
+    print("Book recorded successfully.")
 
 
-wb.save(filename)
+while True:
+    userInput = input()
+
+    if userInput == "new":
+            create_book()
+            wb.save(filename)
+
+    elif userInput == "close":
+        break
+
+    elif userInput.startswith("delete"):
+        subject = userInput.replace("delete ", "")
+        if subject.isnumeric():
+            id = int(subject)
+            sheet.delete_rows(id)
+            wb.save(filename)
+            print("Deleted successfully")
+    else:
+        print("Unkown command.")
+
+
+
 print("done")
