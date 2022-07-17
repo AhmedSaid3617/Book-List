8220a67384c69c02210138e41cd7c769e05392b7
AhmedSaid3617 <said.ahmed3617@gmail.com>
2022-07-17 23:52:36 +0200

    Added commands to edit a registry.

--- Books.py
+++ Books.py
@@ -9,3 +9,19 @@ class Book:
         sheet[f'A{self.order}'] = self.title
         sheet[f'B{self.order}'] = self.author
         sheet[f'C{self.order}'] = self.year
+
+    def rewrite(self, sheet, edit_order, new_title, new_author, new_year):
+        sheet[f'A{edit_order}'] = new_title
+        sheet[f'B{edit_order}'] = new_author
+        sheet[f'C{edit_order}'] = new_year
+
+    def edit_title(self, sheet, edit_order, new_title):
+        sheet[f'A{edit_order}'] = new_title
+
+    def edit_author(self, sheet, edit_order, new_author):
+        sheet[f'B{edit_order}'] = new_author
+
+    def edit_year(self, sheet, edit_order, new_year):
+        sheet[f'C{edit_order}'] = new_year
+
+
--- Registerer.py
+++ Registerer.py
@@ -6,12 +6,6 @@ wb = openpyxl.load_workbook(filename)
 books_sheet = wb.active
 
 
-"""def record(data):
-    books_sheet[f'A{data.id}'] = data.title
-    books_sheet[f'B{data.id}'] = data.author
-    books_sheet[f'C{data.id}'] = data.year"""
-
-
 def create_book():
     max_row = books_sheet.max_row
     title = input("Enter book title:")
@@ -29,35 +23,106 @@ def rundown():
         count += 1
 
 
-rundown()
+def commands_help():
+    print('\n')
+    print("Commands:")
+    print("\"new\": to record a new book.")
+    print("\"rundown\": to displays a list of all books.")
+    print("\"close\": to close application.")
+    print("\"delete {number of book}\": to delete a book.")
+    print("\"rewrite {number of book}\": to rewrite a book\'s details.")
+    print("\"edit title {number of book}\": to edit a book\'s title.")
+    print("\"edit author {number of book}\": to edit a book\'s author.")
+    print("\"edit year {number of book}\": to edit a book\'s year.")
 
-print('\n')
-print("Commands:")
-print("\"new\": to record a new book.")
-print("\"delete {number of book}\": to delete a new book.")
-print("\"rundown\": to displays a list of all books.")
-print("\"close\": to close application.")
+
+rundown()
+commands_help()
 
 while True:
-    userInput = input()
+    user_input = input()
 
-    if userInput == "new":
+    if user_input == "new":
             create_book()
             wb.save(filename)
 
-    elif userInput == "close":
+    elif user_input == "close":
         break
 
-    elif userInput.startswith("delete"):
-        subject = userInput.replace("delete ", "")
+    elif user_input.startswith("delete"):
+        subject = user_input.replace("delete ", "")
         if subject.isnumeric():
-            id = int(subject)
-            books_sheet.delete_rows(id)
-            wb.save(filename)
-            print("Deleted successfully")
+            if int(subject) > 0 and int(subject) <= books_sheet.max_row:
+                id = int(subject)
+                books_sheet.delete_rows(id)
+                wb.save(filename)
+                print("Deleted successfully")
+            else:
+                print(f"Enter a number between 0 and {books_sheet.max_row}")
+        else:
+            print("Please enter a number.")
 
-    elif userInput == "rundown":
+    elif user_input == "rundown":
         rundown()
+
+    elif user_input.startswith("rewrite"):
+        subject = user_input.replace("rewrite ", "")
+        if subject.isnumeric():
+            if int(subject) > 0 and int(subject) <= books_sheet.max_row:
+                order_input = int(subject)
+                input_title = input("Enter new title: ")
+                input_author = input("Enter new author: ")
+                input_year = input("Enter new year: ")
+                Book.rewrite(Book, books_sheet, order_input, input_title, input_author, input_year)
+                wb.save(filename)
+                print(f"Book number {order_input} edited.")
+            else:
+                print(f"Enter a number between 0 and {books_sheet.max_row}")
+        else:
+            print("Please enter a number.")
+
+    elif user_input.startswith("edit title"):
+        subject = user_input.replace("edit title ", "")
+        if subject.isnumeric():
+            if int(subject) > 0 and int(subject) <= books_sheet.max_row:
+                order_input = int(subject)
+                input_title = input("Enter new title: ")
+                Book.edit_title(Book, books_sheet, order_input, input_title)
+                wb.save(filename)
+                print(f"Changed book title number {order_input} to \"{input_title}\"")
+            else:
+                print(f"Enter a number between 0 and {books_sheet.max_row}")
+        else:
+            print("Please enter a number.")
+
+    elif user_input.startswith("edit author"):
+        subject = user_input.replace("edit author ", "")
+        if subject.isnumeric():
+            if int(subject) > 0 and int(subject) <= books_sheet.max_row:
+                order_input = int(subject)
+                input_author = input("Enter new author: ")
+                Book.edit_author(Book, books_sheet, order_input, input_author)
+                wb.save(filename)
+                print(f"Changed book author number {order_input} to \"{input_author}\"")
+            else:
+                print(f"Enter a number between 0 and {books_sheet.max_row}")
+        else:
+            print("Please enter a number.")
+
+    elif user_input.startswith("edit year"):
+        subject = user_input.replace("edit year ", "")
+        if subject.isnumeric():
+            if int(subject) > 0 and int(subject) <= books_sheet.max_row:
+                order_input = int(subject)
+                input_year = input("Enter new year: ")
+                Book.edit_year(Book, books_sheet, order_input, input_year)
+                wb.save(filename)
+                print(f"Changed book year number {order_input} to \"{input_year}\"")
+            else:
+                print(f"Enter a number between 0 and {books_sheet.max_row}")
+        else:
+            print("Please enter a number.")
+
 
     else:
         print("Unkown command.")
